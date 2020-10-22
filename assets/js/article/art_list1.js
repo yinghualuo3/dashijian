410da159246c60e85444f0defdffe86f6388ea21
yinghualuo <1343948723@qq.com>
2020-10-23 00:18:34 +0800

    登录注册2.0

---
+++ assets/js/article/art_list1.js
@@ -0,0 +1,150 @@
+// var layer = layui.layer;
+// var form = layui.form;
+$(function () {
+    // 1.初始化表格
+    getArtList();
+    // 2.初始化分类数据
+    getCateData();
+    // 3.为筛选区表单绑定提交事件(写在入口函数外面)
+    // 4.定义渲染分页的方法
+    renderPageBar();
+
+
+})
+
+// 0.全局变量 默认查询参数对象
+let queryData = {
+    pagenum: 1,
+    pagesize: 2,
+    cate_id: '',  // 文章分类id
+    state: ''
+};
+
+// 1.请求文章列表
+function getArtList() {
+    $.ajax({
+        method: 'GET',
+        url: '/my/article/list',
+        data: queryData,
+        success(res) {
+            console.log(res);
+            if (res.status !== 0) {
+                return layui.layer.msg('获取文章列表失败！');
+            }
+            // 调用模板引擎
+            var strHtml = template('tpl-row', res);
+            $('.layui-table tbody').html(strHtml);
+            // 调用渲染分页的方法
+            renderPageBar(res.total); // res.total总数据条数
+        }
+    })
+}
+
+// 1.1定义美化时间过滤器
+template.defaults.imports.dataFormat = function (date) {
+    const dt = new Date(date)
+
+    var y = dt.getFullYear()
+    var m = padZero(dt.getMonth() + 1);
+    var d = padZero(dt.getDate());
+
+    var hh = padZero(dt.getHours());
+    var mm = padZero(dt.getMinutes());
+    var ss = padZero(dt.getSeconds());
+
+    return y + '-' + m + '-' + d + ' ' + hh + ':' + mm + ':' + ss
+}
+function padZero(time) {
+    return time > 9 ? n : '0' + n;
+}
+
+// 2.初始化文章分类的方法
+function getCateData() {
+    $.ajax({
+        method: 'GET',
+        url: '/my/article/cates',
+        success(res) {
+            if (res.status !== 0) {
+                console.log('获取文章分类列表失败');
+            }
+            // 如果获取文章分类列表成功 则调用模板引擎并得到下拉框的html字符串并重新渲染页面
+            var optStr = template('tpl-select-status', res);
+            // 属性选择器
+            $('[name=selCate]').append(optStr);
+            // 通知layui重新渲染表单区域的ui结构
+            layui.form.render();
+        }
+    })
+}
+
+// 3.为筛选区表单绑定提交事件
+$('#formSearch').on('submit', function (e) {
+    // 3.1阻止表单默认提交行为
+    e.preventDefault();
+    // 3.2获取表单中选项的值 并赋值给查询参数对象queryData中的属性
+    queryData.cate_id = $('#select1').val();
+    queryData.state = $('#select2').val();
+    // console.log(queryData);
+    // 3.3根据最新筛选条件重新渲染表格数据
+    getArtList();
+})
+
+// 4.定义渲染分页的方法
+function renderPageBar(total) {
+    // console.log(total);  //打印条数
+    layui.laypage.render({
+        elem: 'pageBar', // 分页容器的Id 注意不要加#
+        count: total,   // 总数据条款
+        limit: queryData.pagesize,
+        curr: queryData.pagenum,
+        layout: ['count', 'limit', 'prev', 'page', 'next', 'skip'],
+        limits: [2, 3, 5, 10],
+        // 分页切换的时候会触发jump函数
+        jump: function (obj, first) {
+            // 把最新的页码值赋值到queryData查询参数对象的pagenum属性中
+            queryData.pagenum = obj.curr; // 将当前被点击对象设置给全局参数对象里的页码属性
+            // 把最新的条目数 赋值到queryData查询参数对象的pagesize属性中
+            queryData.pagesize = obj.limit; // 将当前选中的页容量设置给全局参数对象
+            //  不是第一次触发就重新请求列表数据
+            if (!first) {
+                getArtList();
+            }
+        }
+    })
+}
+
+$('tbody').on('click', '.btn-del', function () {
+    doDel();
+})
+
+
+// 5.删除列表数据
+function doDel() {
+    // 5.1从被点击按钮上获取当前数据的id
+    var dId = this.dataset.id;
+    console.log(dId);
+    // 删除数据之前 获取当前删除按钮的个数
+    var restRowNum = $('.layui-table tbody tr btn-del').length;
+    // 5.2询问用户是否要删除数据
+    layui.layer.confirm('您确认要删除这条数据吗', { icon: 3, title: '提示' }, function (index) {
+        $.ajax({
+            method: 'GET',
+            url: '/my/article/delete/' + dId,
+            success(res) {
+                layui.layer.msg(res.message);
+                if (res.status == 0) {
+                    if (restRowNum == 1) {
+                        // 当前页码 - 1
+                        queryData.pagenum = queryData.pagenum == 1 ? 1 : queryData.pagenum - 1;
+                    }
+                    // 如果删除成功 则重新调用加载列表方法(刷新表格数据)
+                    // 如果删除失败 则显示错误消息
+                    getArtList();
+                }
+            }
+        })
+        layer.close(index);
+    })
+}
+
+
---
+++ assets/js/login1.js
@@ -0,0 +1,88 @@
+
+var $LoginBox, $RegBox;
+
+$(function () {
+    $LoginBox = $('.login-box');
+    $RegBox = $('.reg-box')
+
+    // 1.dom树加载完毕后 为登录和注册超链接添加点击事件
+    // 1.1为 注册 超链接添加点击事件
+    $('#link_reg').on('click', function () {
+        $LoginBox.hide(); // 隐藏登录框
+        $RegBox.show();   // 显示注册框
+    })
+    // 1.1为 登录 超链接添加点击事件
+    $('#link_login').on('click', function () {
+        $LoginBox.show(); // 显示登录框
+        $RegBox.hide();   // 隐藏注册框
+    })
+
+
+    // 预定义校验规则lay-verify在html页面
+    // 2.自定义校验规则
+    layui.form.verify({
+        // 密码框校验规则
+        pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
+        // 确认密码框校验规则
+        repwd: function (value) { // 此处形参value拿到的是确认密码框中的内容
+            // 获取密码内容
+            var pwdStr = $('.reg-box [name=password]').val() // 密码框中的值
+            // 判断确认密码框value中的内容与密码框val中内容是否一致
+            if (pwdStr !== value) {
+                return '两次密码不一致哦亲~'
+            }
+        }
+    })
+
+    // 3.注册表单提交事件
+    $('#formReg').on('submit', function (e) {
+        // 3.1
+        e.preventDefault();
+        // 3.2获取注册信息 即获取username和password
+        let data = {
+            username: $('.reg-box [name=username]').val().trim(),
+            password: $('.reg-box [name=password]').val().trim()
+        }
+        // 3.3
+        $.post('/api/reguser', data, function (res) {
+            if (res.status !== 0) {
+                layui.layer.msg(res.message)
+            } else {
+                layui.layer.msg(res.message, function () {
+                    // ---------------------不懂---------post ajax-----------------
+                    $('#link_login').click();
+                    // 清空表单内容
+                    $('#formReg')[0].reset();
+                })
+                // 将用户名和密码设置给等领域窗体的输入框
+                $('.login-box [name=username]').val(data.username);
+                $('.login-box [name=password]').val(data.password);
+            }
+        })
+    })
+
+
+    // 4.登录表单提交事件  // 表单而非按钮
+    $('#formLogin').on('submit', function (e) {
+        // 4.1
+        e.preventDefault();
+        // 4.2获取用户名密码数据 serialize()快速获取数据
+        var strData = $(this).serialize();
+        // 4.3
+        $.ajax({
+            method: 'POST',
+            url: '/api/login',
+            data: strData,
+            success(res) {
+                layui.layer.msg(res.message, function () {
+                    if (res.status === 0) {
+                        // 将获取到的token值存到本地
+                        localStorage.setItem('token', res.token);
+                        // 跳转到主页
+                        location.href = 'index.html';
+                    }
+                })
+            }
+        })
+    })
+})
